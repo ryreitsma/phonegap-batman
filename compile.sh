b523efbf46c0cf347977f21379dd13b753b3d9dc
Roelof Reitsma <roelof.reitsma@ogd.nl>
2013-11-11 18:07:26 +0100

    Add compiled javascript and css files to the project
    To be able to make a proper phonegap build, we need the raw source as
    well.

--- compile.sh
+++ compile.sh
@@ -1,8 +1,8 @@
-coffee --compile *.coffee
-coffee --compile controllers/*.coffee
-coffee --compile lib/*.coffee
-coffee --compile models/*.coffee
-coffee --compile views/**/*.coffee
+coffee --compile --output js *.coffee
+coffee --compile --output js controllers/*.coffee
+coffee --compile --output js lib/*.coffee
+coffee --compile --output js models/*.coffee
+coffee --compile --output js views/**/*.coffee 
 echo "Coffeescript compiled successfully!"
 echo "Start compiling scss files!"
 compass compile
